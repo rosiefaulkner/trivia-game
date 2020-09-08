19512b10720efd59b512ac5278d45e9e46cb1a51
Rosie Faulkner <rosiefaulkner@0069L2Z7CRMLCVF.local>
2020-09-08 00:30:52 -0400

    adds effect jquery

--- main.js
+++ main.js
@@ -1,3 +1,28 @@
 $(document).ready(() =>{
-
-});
+    $('.hint-box').on('click', () => {
+      $('.hint').slideToggle(900)
+    })
+  
+    $('.wrong-answer-one').on('click', () => {
+      $('.wrong-text-one').fadeOut('fast');
+      $('.frown').show();
+    })
+  
+    $('.wrong-answer-two').on('click', () => {
+      $('.wrong-text-two').fadeOut('fast');
+      $('.frown').show();
+    })
+  
+    $('.wrong-answer-three').on('click', () => {
+      $('.wrong-text-three').fadeOut('fast');
+      $('.frown').show();
+    })
+  
+    $('.correct-answer').on('click', () => {
+      $('.frown').hide();
+      $('.smiley').show();
+      $('.wrong-text-one').fadeOut('fast');
+      $('.wrong-text-two').fadeOut('fast');
+      $('.wrong-text-three').fadeOut('fast');
+    })
+  });
